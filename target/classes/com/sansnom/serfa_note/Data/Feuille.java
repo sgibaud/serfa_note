7cd26e8fdaee7dc72a081dd43baa4be6bc271b8a
Psylook <nmohaouzine.sandrine@gmail.com>
2024-03-07 09:44:57 +0100

    transfo static en pas static

---
+++ target/classes/com/sansnom/serfa_note/Data/Feuille.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sansnom.serfa_note.Data;
+
+/**
+ *
+ * @author DevInfo
+ */
+public class Feuille {
+    private int id;
+    private String titre;
+    private String text;
+    
+    public Feuille(int id,String titre){
+        this.id= id;
+        this.titre = titre;
+    }
+    public Feuille(int id,String titre, String text){
+        this.id= id;
+        this.titre = titre;
+        this.text = text;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+}
